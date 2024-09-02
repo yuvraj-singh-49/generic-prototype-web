98ebf7d27c1d57edd9e89670f0037d761858c67b
Yuvraj - <yuvraj-@Yuvrajs-Laptop.local>
2024-09-03 02:11:06 +0530

    config-update

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const DEV_BASE_URL = "http://localhost:5849";
 const STG_BASE_URL =
-  "https://pix0n7jenk.execute-api.ap-south-1.amazonaws.com/dev";
+  "http://generic-prototype-backend.ap-south-1.elasticbeanstalk.com/";
 
 export default function axiosApi(header?: any) {
   return axios.create({
